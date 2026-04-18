0c9f9186a5e43138c3f5cd10cc7a03b1c15af490
bisolasonde <bisolasonde@gmail.com>
2026-04-18 11:48:08 -0500

    gcp changes

--- 01-Introduction/terraform files/variables.tf
+++ 01-Introduction/terraform files/variables.tf
@@ -1,6 +1,6 @@
-# This replaces the AWS project/region logic
+
 variable "gcp_project_id" {
-  gcp_project_id = "enterpriseai-459922"
+  deffault = "enterpriseai-459922"
   type        = string
 }
 
