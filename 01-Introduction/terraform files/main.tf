6f8ad4023b62245189cf5a1030eac0a972903f28
bisolasonde <bisolasonde@gmail.com>
2026-04-18 11:01:48 -0500

    gcp changes

--- 01-Introduction/terraform files/main.tf
+++ 01-Introduction/terraform files/main.tf
@@ -1,16 +1,23 @@
-# 1. ANSIBLE CONTROL NODE
+# 1. THE CONTROL NODE (Ansible-Ubuntu)
 resource "google_compute_instance" "ansible_ubuntu" {
   name         = "ansible-ubuntu"
-  machine_type = var.instance_type
-  zone         = var.zone
+  machine_type = var.my_instance_type
+  zone         = var.gcp_region == "us-central1" ? "us-central1-a" : "${var.gcp_region}-a"
 
   boot_disk {
-    initialize_params { image = "ubuntu-os-cloud/ubuntu-2204-lts" }
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
   }
 
   network_interface {
     network = "default"
-    access_config {} 
+    access_config {} # Assigns a Public IP
+  }
+
+  # This creates the 'ansible' user and injects your MacBook's key
+  metadata = {
+    ssh-keys = "ansible:${file(var.my_key)}"
   }
 
   metadata_startup_script = <<EOF
@@ -18,19 +25,19 @@ resource "google_compute_instance" "ansible_ubuntu" {
 ${file("${path.module}/ansible-install-ubuntu.sh")}
 ${file("${path.module}/vscode-install.sh")}
 EOF
-
-ssh-keys = "ansible:${file(var.my_key)}"
 }
 
-# 2. TWO UBUNTU MANAGED HOSTS
+# 2. THE MANAGED UBUNTU NODES (Count = 2)
 resource "google_compute_instance" "ubuntu_hosts" {
   count        = 2
   name         = "my-ubuntu-${count.index}"
-  machine_type = var.instance_type
-  zone         = var.zone
+  machine_type = var.my_instance_type
+  zone         = var.gcp_region == "us-central1" ? "us-central1-a" : "${var.gcp_region}-a"
 
   boot_disk {
-    initialize_params { image = "ubuntu-os-cloud/ubuntu-2204-lts" }
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
   }
 
   network_interface {
@@ -38,18 +45,23 @@ resource "google_compute_instance" "ubuntu_hosts" {
     access_config {}
   }
 
+  metadata = {
+    ssh-keys = "ansible:${file(var.my_key)}"
+  }
+
   metadata_startup_script = file("${path.module}/create_ansible_user.sh")
-  ssh-keys = "ansible:${file(var.my_key)}"
 }
 
-# 3. ONE RHEL MANAGED HOST
+# 3. THE MANAGED RHEL NODE (Count = 1)
 resource "google_compute_instance" "rhel_hosts" {
   name         = "my-rhel-0"
-  machine_type = var.instance_type
-  zone         = var.zone
+  machine_type = var.my_instance_type
+  zone         = var.gcp_region == "us-central1" ? "us-central1-a" : "${var.gcp_region}-a"
 
   boot_disk {
-    initialize_params { image = "rhel-cloud/rhel-9" }
+    initialize_params {
+      image = "rhel-cloud/rhel-9"
+    }
   }
 
   network_interface {
@@ -57,8 +69,24 @@ resource "google_compute_instance" "rhel_hosts" {
     access_config {}
   }
 
+  metadata = {
+    ssh-keys = "ansible:${file(var.my_key)}"
+  }
+
   metadata_startup_script = file("${path.module}/create_ansible_user.sh")
-  ssh-keys = "ansible:${file(var.my_key)}"
+}
+
+# 4. FIREWALL RULE (To allow you to SSH from your Mac)
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh-ansible-lab"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # For learning; restrict to your IP for better security
 }
 
 
