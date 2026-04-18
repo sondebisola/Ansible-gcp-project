07c5fb03a59c0b0ae22afa98b4b72bdd5430d6ca
bisolasonde <bisolasonde@gmail.com>
2026-04-18 11:33:55 -0500

    gcp changes

--- 01-Introduction/terraform files/outputs.tf
+++ 01-Introduction/terraform files/outputs.tf
@@ -1,27 +1,23 @@
-# Terraform Output Values
-output "ansible_publicIp" {
-  description = "EC2 Instance Public IP"
-  value       = aws_instance.ubuntu.public_ip
+# 1. Ansible Control Node Public IP
+output "control_node_public_ip" {
+  description = "The public IP of the Ansible Control Node"
+  value       = google_compute_instance.ansible_ubuntu.network_interface.0.access_config.0.nat_ip
 }
 
-output "rhel_publicIp" {
-  description = "EC2 Instance Public IP"
-  value       = aws_instance.rhel-hosts[*].public_ip
+# 2. Managed Ubuntu Nodes Public IPs
+output "ubuntu_hosts_public_ips" {
+  description = "The public IPs of the managed Ubuntu nodes"
+  value       = google_compute_instance.ubuntu_hosts[*].network_interface.0.access_config.0.nat_ip
 }
 
-output "rhel_privateIp" {
-  description = "EC2 Instance Public IP"
-  value       = aws_instance.rhel-hosts[*].private_ip
+# 3. Managed RHEL Node Public IP
+output "rhel_host_public_ip" {
+  description = "The public IP of the managed RHEL node"
+  value       = google_compute_instance.rhel_hosts.network_interface.0.access_config.0.nat_ip
 }
 
-# EC2 Instance Public IP
-output "instance_publicIp" {
-  description = "EC2 Instance Public IP"
-  value       = aws_instance.ubuntu-hosts[*].public_ip
-}
-
-# EC2 Instance Public DNS
-output "instance_privateIp" {
-  description = "EC2 Instance Private IP"
-  value       = aws_instance.ubuntu-hosts[*].private_ip
+# 4. Internal IPs (Useful for Ansible communication)
+output "control_node_internal_ip" {
+  description = "The internal IP of the Control Node"
+  value       = google_compute_instance.ansible_ubuntu.network_interface.0.network_ip
 }
